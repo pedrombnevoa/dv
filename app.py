cfb3fecff641c0b9f59a049e7a91b9ac47f97049
pedrombnevoa <1325@oPo$>
2020-05-12 18:54:35 +0100

    Test1

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1(children='Test'),
+    html.H1(children='Test1'),
 
     html.Div(children='''
         Example of html Container
