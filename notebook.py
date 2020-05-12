493dbe798e4bd8cf2500f2f6e426e69ef0f6b5cb
Pedro Nevoa <Pedro.Nevoa@unit4.com>
2020-05-12 16:58:45 +0100

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
 # ds = 'https://media.githubusercontent.com/media/pedrombnevoa/dv/master/globalterrorismdb_0718dist.csv'
 ds = os.getcwd() + '\globalterrorismdb_0718dist.csv'
@@ -7,4 +10,23 @@ ds = os.getcwd() + '\globalterrorismdb_0718dist.csv'
 fields = ['eventid', 'iyear', 'country_txt', 'region_txt', 'city', 'latitude', 'longitude', 'nkill']
 
 df = pd.read_csv(ds, encoding='ISO-8859-1', usecols=fields)
-print(df.head(1))
+
+app = dash.Dash(__name__)
+
+server = app.server
+
+app.layout = html.Div(children=[
+    html.H1(children='My First DashBoard'),
+
+    html.Div(children='''
+        Example of html Container
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        #figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
