da256ec3f67fbb9b8135ed1dda8dec937c86407c
Vitor Miguel Fernandes <vitormdfernandes@gmail.com>
2020-06-10 20:00:01 +0100

    added grouped gang kills, added graph added style

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import os
+import deathsvstotaldeathsChart as deathsvstotaldeaths
 
 ds = os.getcwd() + '\globalterrorismdb_0718dist.csv'
 
@@ -112,6 +113,11 @@ columns = ['Year','Organization','NumDeaths']
 
 killsByGangDF = pd.read_csv(YearGangKillsPath, encoding='ISO-8859-1', usecols=columns)
 
+killsByRegionDFPath = os.getcwd() + '\GroupedYearRegionKills.csv'
+
+columnskillsByRegion = ['Region','Year','NumDeaths']
+#Region,Year,NumDeaths
+killsByRegionDF = pd.read_csv(killsByRegionDFPath, encoding='ISO-8859-1', usecols=columnskillsByRegion)
 
 app = dash.Dash(__name__, assets_folder='style')
 server = app.server
@@ -137,7 +143,49 @@ app.layout = html.Div(children=[
                              template='plotly_dark').update_layout(
                                        {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
                                         'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-                                        )
+                                        ),
+
+    dcc.Graph(
+            id='fig2',
+            figure=deathsvstotaldeaths.DeathOverDeathFig,
+            animate=False
+        ),
+
+    dcc.Graph(id='killsByGang',
+              config={'displayModeBar': False},
+              animate=True,
+              figure=px.line(killsByGangDF,
+                             x='Year',
+                             y='NumDeaths',
+                             color='Organization',
+                             template='plotly_dark').update_layout(
+                                       {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                                        'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+                                        ),
+    dcc.Graph(id='killsByRegion',
+              config={'displayModeBar': False},
+              animate=True,
+              figure=px.line(killsByRegionDF,
+                             x='Year',
+                             y='NumDeaths',
+                             color='Region',
+                             template='plotly_dark').update_layout(
+                                       {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                                        'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+                                        ),
+    dcc.Graph(id='attackvsdeaths',
+              config={'displayModeBar': False},
+              animate=True,
+              figure=px.line(deathsvstotaldeaths.RegionYearAttackDeathsDF,
+                             x='NumDeathsComul',
+                             y='NumDeathsPerYear',
+                             color='Region',
+                             template='plotly_dark').update_layout(
+                                       {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                                        'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+                                        ),
+
+
 ])
 
 
